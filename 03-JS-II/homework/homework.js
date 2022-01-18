289b4a80e143d2aa2efea87174af27dd1ee65d4a
gemaiolino <gonzalomaiolino@live.com>
2022-01-17 23:37:49 -0300

    primeros 5 ej.

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -68,23 +68,47 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
-}
+  switch (color){
+    case "blue":
+    return "This is blue"
+    case "red":
+      return "This is red"
+      case "green":
+        return "This is green"
+        case "orange":
+          return "This is orange"
+          default :
+          return "Color not found"
+          
+  }
+  
+
+ 
+}  
+
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (numero === 10 || 5){
+  if (numero === 5 || numero === 10){
     return true
-  }else if (numero !== 10 || 5){
+  }else{
     return false
   }
+  
+  
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (numero < 50 && numero > 20){
+    return true
+  } else {
+    return false
+  }
 }
 
 function esEntero(numero) {
@@ -95,6 +119,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+ return numero % 1 === 0;
 }
 
 function fizzBuzz(numero) {
@@ -102,6 +127,16 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+  if (numero % 15 === 0){
+    return "fizzbuzz"
+  }if (numero % 3 === 0){
+    return "fizz"
+    }else if (numero % 5 === 0){
+      return "buzz"
+    }else{
+      return numero
+    }
+   
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -138,6 +173,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+
   
 }
 
